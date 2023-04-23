b502c9c08fa32f91c1f85de770ac531bdcc86696
Janavi S <96911037+Janavi-S05@users.noreply.github.com>
2023-04-23 16:53:08 +0530

    Create postModel.js

---
+++ backend/models/postModel.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const Schema=mongoose.Schema;
+
+const postSchema=new Schema({
+   content:{type:String,trim:true},
+   postedBy:{type:Schema.Types.ObjectId,ref:'User'},
+   pinned: Boolean,
+   profilePic:{
+      type:String,
+      default:"/images/profilePic.png"
+  },
+   likes:[{type:Schema.Types.ObjectId, ref:'User'}],
+   retweetUsers:[{type:Schema.Types.ObjectId,ref:'User'}],
+   retweetData:{type:Schema.Types.ObjectId,ref:'Post'},
+   replyTo:{type:Schema.Types.ObjectId,ref:'Post'},
+  
+
+
+},{timestamps:true});
+
+const Post=mongoose.model('Post',postSchema);
+export default Post;
