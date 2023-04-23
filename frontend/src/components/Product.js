83f13ae1d1d1740e2838adbbe4c96edc45fff58a
Janavi S <96911037+Janavi-S05@users.noreply.github.com>
2023-04-23 16:57:42 +0530

    Update Product.js

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -33,20 +33,35 @@ function Product(props){
 
 
     return(
-        <div >
+        <div  className="w-72 bg-white rounded-lg shadow-md mx-auto drop-shadow-md">
+            {/* <img
+      src="https://via.placeholder.com/640x480"
+      alt="Product image"
+      className="w-full h-64 object-cover rounded-t-lg"
+    />
+    <div className="p-5">
+      <h1 className="text-2xl font-bold mb-2">Product Title</h1>
+      <p className="text-gray-600 text-lg mb-5">Price: $99.99</p>
+      <a
+        href="#"
+        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors"
+      >
+        View details
+      </a> */}
+  
 
         <Link to={`/product/${product.slug}`}>
-        <img src={product.image} className="card-img-top" alt={product.name} />
+        <img src={product.image} className="w-full h-52 object-fit rounded-t-lg" alt={product.name} />
         </Link>
 
-        <div>
+        <div className="py-2 px-3">
             <Link to={`/product/${product.slug}`}>
-            <h1>{product.name}</h1>
+            <h1 className="text-2xl font-bold mb-2 ">{product.name}</h1>
             </Link>
            
             <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
-            <Card.Text>${product.price}</Card.Text>
-            <Button onClick={()=>addToCartHandler(product)}>Add to cart</Button>
+            <h2 className="text-gray-600 text-lg mb-1">${product.price}</h2>
+            <div className="justify-center flex pb-2"><Button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors " onClick={()=>addToCartHandler(product)}>Add to cart</Button></div>
           
 
         </div>
