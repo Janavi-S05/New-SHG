e7f108b482349ef46a03de7e719078d3b3b1f204
Janavi S <96911037+Janavi-S05@users.noreply.github.com>
2023-04-23 16:57:19 +0530

    Update CheckoutSteps.js

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -3,10 +3,11 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 export default function CheckoutSteps(props)
 {
-    return <Row className="checkout-steps">
-        <Col className={props.step1 ? 'active' : ''}>Sign-In</Col>
-        <Col className={props.step2 ? 'active' : ''}>Shipping</Col>
-        <Col className={props.step3 ? 'active' : ''}>Payment</Col>
-        <Col className={props.step4 ? 'active' : ''}>Place Order</Col>
+    // my-8 w-6/12 mx-auto
+    return <Row className="checkout-steps container small-container my-8 mx-auto pl-0">
+        <Col className={props.step1 ? 'active' : ''} ><h1 className="text-lg pb-3">Sign-In</h1></Col>
+        <Col className={props.step2 ? 'active' : ''}><h1 className="text-lg">Shipping</h1></Col>
+        <Col className={props.step3 ? 'active' : ''}><h1 className="text-lg">Payment</h1></Col>
+        <Col className={props.step4 ? 'active' : ''}><h1 className="text-lg ">Place Order</h1></Col>
     </Row>
 }
