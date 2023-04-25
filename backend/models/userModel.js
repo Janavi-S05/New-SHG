733e860a5f27ede5103bbafd1ca489040ead4103
Janavi S <96911037+Janavi-S05@users.noreply.github.com>
2023-04-25 19:18:45 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,15 @@ const userSchema=new mongoose.Schema(
         name:{type:String,required:true},
         email:{type:String,required:true,unique:true},
         password:{type:String,required:true},
+        profilePic:{
+            type:String,
+            default:"/images/profilePic.png"
+        },
+        userbio:[{
+            type:String,
+        }],
+        followers:[{type:mongoose.Schema.Types.ObjectId,ref:'User'}],
+        following:[{type:mongoose.Schema.Types.ObjectId,ref:'User'}],
         isAdmin:{type:Boolean,default:false,required:true},
        
     },
