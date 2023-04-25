c11dda07a6ba288097d355400b69ee7912f41895
Janavi S <96911037+Janavi-S05@users.noreply.github.com>
2023-04-25 19:28:55 +0530

    Update ProductDescriptionScreen.js

--- frontend/src/screens/ProductDescriptionScreen.js
+++ frontend/src/screens/ProductDescriptionScreen.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import Row from 'react-bootstrap/Row';
@@ -10,13 +10,24 @@ import Product from "../components/Product";
 import { useEffect } from "react";
 import { useReducer } from "react";
 import logger from 'use-reducer-logger';
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import Rating from "../components/Rating";
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+import { Button } from "react-bootstrap";
+import { Store } from "../Store";
+
+
+
 
 const reducer = (state, action) => {
     switch (action.type) {
         case 'FETCH_REQUEST':
             return { ...state, loading: true };
         case 'FETCH_SUCCESS':
-            return { ...state, products: action.payload, loading: false };
+            return { ...state, product: action.payload, loading: false };
         case 'FETCH_FAIL':
             return { ...state, loading: false, error: action.payload };
         default:
@@ -24,13 +35,19 @@ const reducer = (state, action) => {
     }
 }
 
-function ProductScreen() {
+function ProductDescriptionScreen() {
+    const navigate = useNavigate();
+    const params = useParams();
+    const { slug } = params;
 
-    const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
-        products: [],
+
+
+    const [{ loading, error, product }, dispatch] = useReducer(logger(reducer), {
+        product: [],
         loading: true,
         error: '',
     })
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -38,78 +55,126 @@ function ProductScreen() {
             dispatch({ type: 'FETCH_REQUEST' });
 
             try {
-                const result = await axios.get('/api/products');
+                const result = await axios.get(`/api/products/slug/${slug}`);
                 dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
             }
 
             catch (err) {
                 dispatch({ type: 'FETCH_FAIL', payload: err.message });
             }
+
+
+
         };
         fetchData();
-    }, []);
-    console.log(products);
+    }, [slug]);
 
-    return (
-        <div>
-            <div className="flex justify-center my-5">
-                <h1 className="text-2xl font-medium border-b-2 text-slate-500">Featured Products</h1>
-                {/*<div class="container mx-auto mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10"> */}
-            </div>
 
-                    <div  className="container">
-                        {/* {
-        
-        loading? (<LoadingBox/>)
-        :error? (<MessageBox variant="danger">{error} </MessageBox>):(
-            <Row className="container mx-auto mt-10 grid grid-cols-1 sm:grid-cols-2 sm:grid-cols-3 gap-10">
-        {products.map((product)=>(
-        <Col key={product.slug}  className="object-cover bg-white rounded-lg shadow-md ml-auto">
-            <Product product={product}></Product>
-        </Col>
-        ))}
-        </Row>
-        )
-        
-      } */}
-                        {/* <div className="container mx-auto mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10"> */}
-                        {/* Product item 1 */}
-                        {/* <div className="w-80 bg-white rounded-lg shadow-md mx-auto"> */}
-                        {/* <img
-      src="https://via.placeholder.com/640x480"
-      alt="Product image"
-      className="w-full h-64 object-cover rounded-t-lg"
-    />
-    <div className="p-5">
-      <h1 className="text-2xl font-bold mb-2">Product Title</h1>
-      <p className="text-gray-600 text-lg mb-5">Price: $99.99</p>
-      <a
-        href="#"
-        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors"
-      >
-        View details
-      </a>
-    </div> */}
-                        {
+    const { state, dispatch: ctxDispatch } = useContext(Store);
+    const { cart } = state
+    const addToCartHandler = async (item) => {
+        const existItem = cart.cartItems.find((x) => x.id === product.id);
+        const quantity = existItem ? existItem.quantity + 1 : 1
+        const { data } = await axios.get(`/api/products/${product.id}`)
+        if (data.countInStock < quantity) {
+            window.alert('Sorry.Product is out of stock');
+            return;
+        }
+        ctxDispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } })
 
-                            loading ? (<LoadingBox />)
-                                : error ? (<MessageBox variant="danger">{error} </MessageBox>) : (
-                                    <Row>
-                                        {products.map((product) => (
-                                            <Col key={product.slug} className="pb-8">
-                                                <Product product={product}></Product>
-                                            </Col>
-                                        ))}
+        navigate('/cart')
+    }
+
+
+
+
+    return loading ? (<LoadingBox />)
+        : error ? (<MessageBox variant="danger">{error} </MessageBox>) : (
+
+            <Row className="items-center justify-center">
+                <Col md={5}>
+                    <img
+                        className="w-50 items-center mx-auto"
+                        src={product.image}
+                        alt={product.name}
+                    >
+
+                    </img>
+                </Col>
+                <Col md={6} >
+                    <ListGroup variant="flush">
+                        {/* <ListGroup.Item>
+
+                            <h1>{product.name}</h1>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <Rating
+                                rating={product.rating}
+                                numReviews={product.numReviews}
+                            />
+                        </ListGroup.Item>
+                        <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
+                        <ListGroup.Item>
+                            Description:
+                            <p>{product.description}</p>
+                        </ListGroup.Item> */}
+                        <div class="w-full lg:w-3/4 lg:pl-10 mt-10 lg:mt-0">
+                            <h1 class="text-3xl font-bold mb-3 mt-5">{product.name}</h1>
+                            <Rating
+                                rating={product.rating}
+                                numReviews={product.numReviews}
+                            />
+                            <p class="text-gray-600 text-lg mb-2">Price : ${product.price}</p>
+                            <p class="text-gray-600 leading-loose mb-2 text-justify">{product.description}</p>
+                        </div>
+                    </ListGroup>
+
+                    <Card className="w-50 ml-10">
+                        <Card.Body>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>
+                                    <Row >
+                                        <Col>Price:</Col>
+                                        <Col>${product.price}</Col>
                                     </Row>
-                                )
 
-                        }
-                    </div>
-                </div>
-            // </div>
-        // </div>
-        // </div>
-    )
+                                </ListGroup.Item>
+
+                                <ListGroup.Item>
+                                    <div>
+                                        <Col>Status:</Col>
+                                        <Col>{product.countInStock > 0 ? (
+                                            <Badge bg="success">In Stock</Badge>
+                                        ) : (
+                                            <Badge bg="danger">Out of Stock</Badge>
+                                        )}</Col>
+                                    </div>
+                                </ListGroup.Item>
+
+                                {product.countInStock > 0 && (
+                                    <ListGroup.Item>
+                                        <div className="d-grid">
+                                            <Button onClick={addToCartHandler} variant="primary">
+                                                Add To Cart
+                                            </Button>
+                                        </div>
+                                    </ListGroup.Item>
+                                )}
+                            </ListGroup>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+
+
+            </Row>
+
+        )
+
+
+
+
 }
 
-export default ProductScreen;
+export default ProductDescriptionScreen;
