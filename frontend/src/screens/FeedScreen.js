bf2b52ac22a07ba8d0017f3fccca70bab05aa739
Janavi S <96911037+Janavi-S05@users.noreply.github.com>
2023-04-25 19:27:29 +0530

    Update FeedScreen.js

--- frontend/src/screens/FeedScreen.js
+++ frontend/src/screens/FeedScreen.js
@@ -123,44 +123,16 @@ export default function FeedScreen() {
                                     </div>
                                     <div class="flex items-center justify-between">
                                         <div class="flex items-center pr-5">
-                                            {/* <label for="image-upload" class="cursor-pointer mr-3">
-                                            </label>
-                                            <input id="image-upload" type="file" class="hidden"></input> */}
+                                            
                                             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Post</button>
                                         </div>
                                         <div class="flex items-center">
-                                            {/* <span class="text-gray-600 mr-3">Add to your post:</span>
-                                            <button class="bg-gray-200 hover:bg-gray-300 rounded-full p-2" type="button">
-                                                <svg class="w-6 h-6 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-                                                </svg>
-                                            </button> */}
-                                            {/* <button class="bg-gray-200 hover:bg-gray-300 rounded-full p-2 mx-1" type="button">
-                                                <svg class="w-6 h-6 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 
-                      24" xmlns="http://www.w3.org/2000/svg">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
-                                                </svg>
-                                            </button>
-                                            <button class="bg-gray-200 hover:bg-gray-300 rounded-full p-2" type="button">
-                                                <svg class="w-6 h-6 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-                                                </svg>
-                                            </button> */}
+                                    
                                         </div>
                                     </div>
                                 </form>
 
                             </div>
-                            {/* <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                                <h2 class="text-lg font-bold mb-4">Recent Posts</h2>
-                                <div class="border-b border-gray-200 mb-4">
-                                    <div class="flex items-center">
-                                        <img class="w-10 h-10 rounded-full mr-4" src="https://via.placeholder.com/150" alt="Avatar"></img>
-                                        <div>
-                                            <span class="font-bold text-gray-700">John Doe</span>
-                                            <span class="text-gray-600 text-sm"> on April 15th, 2023</span>
-                                        </div>
-                                    </div> */}
                             <div class="mt-2">
                                 {reversedposts.map((post) => (
 
@@ -175,28 +147,15 @@ export default function FeedScreen() {
                                                         <span class="text-gray-600 text-sm"> on April 15th, 2023</span>
                                                     </div>
                                                 </div>
-                                                {/* <div class="flex items-center">
-                                                    <img class="w-10 h-10 rounded-full mr-4" src="https://via.placeholder.com/150" alt="Avatar"></img>
-                                                    <div>
-                                                        <span class="font-bold text-gray-700">John Doe</span>
-                                                        <span class="text-gray-600 text-sm"> on April 15th, 2023</span>
-                                                    </div>
-                                                </div> */}
                                             </div>
                                             <PostComponent key={post._id} post={post}></PostComponent>
                                         </div>
-                                        {/* <PostComponent key={post._id} post={post}></PostComponent> */}
+
                                     </Col>
 
                                 ))}
                             </div>
-                            {/* <div class="flex justify-between items-center mt-4">
-                                <div class="flex items-center">
-                                </div>
-                            </div> */}
-                            {/* </div> */}
-
-                            {/* </div> */}
+            
                         </div>
                     </div>
                 </div>
