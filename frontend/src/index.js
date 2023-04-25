d072ab3eda0b141c9e5f686f9dfa3e24118bc28d
Janavi S <96911037+Janavi-S05@users.noreply.github.com>
2023-04-25 19:31:04 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Store } from './Store';
 import { StoreProvider } from './Store';
+// import './tailwind.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
